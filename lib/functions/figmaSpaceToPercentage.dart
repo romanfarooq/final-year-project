ecc16dd67572aae5904e03eabe9c289c4dc92ff0
Chaudhary Muhammad Waqar Hussain <chmwaqarhussaingujjar@gmail.com>
2024-03-30 15:24:30 +0500

    workshop_signup

--- lib/functions/figmaSpaceToPercentage.dart
+++ lib/functions/figmaSpaceToPercentage.dart
@@ -9,3 +9,14 @@ double figmaSpaceToPercentage(double figmaSpace, BuildContext context) {
   // Calculate the equivalent space in Flutter using MediaQuery
   return (MediaQuery.of(context).size.height * spacePercentage) / 100;
 }
+
+double figmaSpaceToPercentageWidth(double figmaSpace, BuildContext context) {
+  // Measure the width of the device screen
+  double screenWidth = MediaQuery.of(context).size.width;
+
+  // Calculate the percentage of the screen width occupied by the Figma space
+  double spacePercentageWidth = (figmaSpace / screenWidth) * 100;
+
+  // Calculate the equivalent space in Flutter using MediaQuery for width
+  return (MediaQuery.of(context).size.width * spacePercentageWidth) / 100;
+}
