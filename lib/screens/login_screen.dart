ed6b6415ae8c2948bd8fe2e1d9f564958c6473b5
Roman Farooq <theromanfarooq@gmail.com>
2024-04-07 01:29:28 +0500

    added SingleChildScrollView to login & signup screen. Also now subtracting pdding top from screen height

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -28,165 +28,172 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
+    final height = MediaQuery.of(context).size.height;
+    final paddingTop = MediaQuery.of(context).padding.top;
+    final screenHeight = height - paddingTop;
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Form(
-          key: _formKey,
+        child: SingleChildScrollView(
           child: Container(
             width: screenWidth,
+            height: screenHeight,
             padding: EdgeInsets.symmetric(
               horizontal: screenWidth * 0.05,
               vertical: screenHeight * 0.01,
             ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: screenHeight * 0.01),
-                Image.asset(
-                  ImageConstant.carcare2,
-                  height: screenHeight * 0.2,
-                  width: screenWidth * 0.4,
-                  alignment: Alignment.center,
-                ),
-                SizedBox(height: screenHeight * 0.01),
-                Padding(
-                  padding: EdgeInsets.only(left: screenWidth * 0.05),
-                  child: Text(
-                    "Login ",
-                    style: Theme.of(context).textTheme.titleLarge,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: screenHeight * 0.01),
+                  Image.asset(
+                    ImageConstant.carcare2,
+                    height: screenHeight * 0.2,
+                    width: screenWidth * 0.4,
+                    alignment: Alignment.center,
                   ),
-                ),
-                SizedBox(height: screenHeight * 0.01),
-                Padding(
-                  padding: EdgeInsets.only(left: screenWidth * 0.02),
-                  child: Text(
-                    "Welcome to Car Car Pro",
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyMedium!
-                        .copyWith(fontSize: 12),
-                  ),
-                ),
-                SizedBox(height: screenHeight * 0.02),
-                CustomTextFormField(
-                  controller: _userNameController,
-                  hintText: "Username",
-                  prefix: Container(
-                    margin: EdgeInsets.fromLTRB(
-                      screenWidth * 0.02,
-                      screenHeight * 0.01,
-                      screenWidth * 0.04,
-                      screenHeight * 0.01,
-                    ),
-                    child: SvgPicture.asset(
-                      ImageConstant.profile,
-                      height: screenHeight * 0.03,
-                      width: screenWidth * 0.06,
+                  SizedBox(height: screenHeight * 0.03),
+                  Padding(
+                    padding: EdgeInsets.only(left: screenWidth * 0.05),
+                    child: Text(
+                      "Login ",
+                      style: Theme.of(context).textTheme.titleLarge,
                     ),
                   ),
-                  prefixConstraints: BoxConstraints(
-                    maxHeight: screenHeight * 0.1,
-                  ),
-                ),
-                SizedBox(height: screenHeight * 0.02),
-                CustomTextFormField(
-                  controller: _passwordController,
-                  hintText: "Password",
-                  textInputAction: TextInputAction.done,
-                  textInputType: TextInputType.visiblePassword,
-                  prefix: Container(
-                    margin: EdgeInsets.fromLTRB(
-                      screenWidth * 0.02,
-                      screenHeight * 0.01,
-                      screenWidth * 0.04,
-                      screenHeight * 0.01,
-                    ),
-                    child: SvgPicture.asset(
-                      ImageConstant.lockpasswordline,
-                      fit: BoxFit.cover,
-                      height: screenHeight * 0.03,
-                      width: screenWidth * 0.06,
+                  SizedBox(height: screenHeight * 0.01),
+                  Padding(
+                    padding: EdgeInsets.only(left: screenWidth * 0.02),
+                    child: Text(
+                      "Welcome to Car Car Pro",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium!
+                          .copyWith(fontSize: 12),
                     ),
                   ),
-                  prefixConstraints: BoxConstraints(
-                    maxHeight: screenHeight * 0.1,
+                  SizedBox(height: screenHeight * 0.02),
+                  CustomTextFormField(
+                    controller: _userNameController,
+                    hintText: "Username",
+                    prefix: Container(
+                      margin: EdgeInsets.fromLTRB(
+                        screenWidth * 0.02,
+                        screenHeight * 0.01,
+                        screenWidth * 0.04,
+                        screenHeight * 0.01,
+                      ),
+                      child: SvgPicture.asset(
+                        ImageConstant.profile,
+                        height: screenHeight * 0.03,
+                        width: screenWidth * 0.06,
+                      ),
+                    ),
+                    prefixConstraints: BoxConstraints(
+                      maxHeight: screenHeight * 0.1,
+                    ),
                   ),
-                  obscureText: true,
-                ),
-                SizedBox(height: screenHeight * 0.03),
-                Text(
-                  "Forgot Password?",
-                  style: Theme.of(context).textTheme.titleSmall,
-                ),
-                SizedBox(height: screenHeight * 0.03),
-                CustomElevatedButton(
-                  text: "Login",
-                  buttonStyle: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.transparent),
-                    elevation: MaterialStateProperty.all<double>(0),
+                  SizedBox(height: screenHeight * 0.02),
+                  CustomTextFormField(
+                    controller: _passwordController,
+                    hintText: "Password",
+                    textInputAction: TextInputAction.done,
+                    textInputType: TextInputType.visiblePassword,
+                    prefix: Container(
+                      margin: EdgeInsets.fromLTRB(
+                        screenWidth * 0.02,
+                        screenHeight * 0.01,
+                        screenWidth * 0.04,
+                        screenHeight * 0.01,
+                      ),
+                      child: SvgPicture.asset(
+                        ImageConstant.lockpasswordline,
+                        fit: BoxFit.cover,
+                        height: screenHeight * 0.03,
+                        width: screenWidth * 0.06,
+                      ),
+                    ),
+                    prefixConstraints: BoxConstraints(
+                      maxHeight: screenHeight * 0.1,
+                    ),
+                    obscureText: true,
                   ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(screenWidth * 0.05),
-                    gradient: LinearGradient(
-                      begin: const Alignment(0.5, 0),
-                      end: const Alignment(0.5, 1),
-                      colors: [
-                        const Color(0XFF6E7CB9),
-                        Theme.of(context).colorScheme.primary,
+                  SizedBox(height: screenHeight * 0.03),
+                  Text(
+                    "Forgot Password?",
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  SizedBox(height: screenHeight * 0.03),
+                  CustomElevatedButton(
+                    text: "Login",
+                    buttonStyle: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.transparent),
+                      elevation: MaterialStateProperty.all<double>(0),
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(screenWidth * 0.05),
+                      gradient: LinearGradient(
+                        begin: const Alignment(0.5, 0),
+                        end: const Alignment(0.5, 1),
+                        colors: [
+                          const Color(0XFF6E7CB9),
+                          Theme.of(context).colorScheme.primary,
+                        ],
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context)
+                          .pushReplacementNamed(AppRoutes.selectUserScreen);
+                    },
+                  ),
+                  SizedBox(height: screenHeight * 0.02),
+                  Padding(
+                    padding: EdgeInsets.only(top: screenHeight * 0.02),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don’t have an account?",
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleSmall!
+                              .copyWith(
+                                color: const Color(0XFF040415).withOpacity(0.4),
+                              ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: screenWidth * 0.02),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.of(context)
+                                  .pushReplacementNamed(AppRoutes.signUpScreen);
+                            },
+                            child: Text(
+                              "Sign Up",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .copyWith(
+                                    color: const Color(0XFFFF5B00),
+                                  ),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
-                  onPressed: () {
-                    Navigator.of(context)
-                        .pushReplacementNamed(AppRoutes.selectUserScreen);
-                  },
-                ),
-                SizedBox(height: screenHeight * 0.02),
-                Padding(
-                  padding: EdgeInsets.only(top: screenHeight * 0.02),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Don’t have an account?",
-                        style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                              color: const Color(0XFF040415).withOpacity(0.4),
-                            ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: screenWidth * 0.02),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.of(context)
-                                .pushReplacementNamed(AppRoutes.signUpScreen);
-                          },
-                          child: Text(
-                            "Sign Up",
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleSmall!
-                                .copyWith(
-                                  color: const Color(0XFFFF5B00),
-                                ),
-                          ),
-                        ),
-                      ),
-                    ],
+                  const Spacer(),
+                  Image.asset(
+                    ImageConstant.carcare1,
+                    height: screenHeight * 0.15,
+                    width: screenWidth * 0.3,
+                    alignment: Alignment.center,
                   ),
-                ),
-                const Spacer(),
-                Image.asset(
-                  ImageConstant.carcare1,
-                  height: screenHeight * 0.15,
-                  width: screenWidth * 0.3,
-                  alignment: Alignment.center,
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
