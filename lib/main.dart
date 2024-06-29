bc5d44c0871deb3c1dcbd1036619b9409c907ea6
Roman Farooq <theromanfarooq@gmail.com>
2024-06-29 10:56:46 +0500

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:car_care/screens/login_screen.dart';
-import 'package:car_care/screens/signup_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 import './models/car_info.dart';
 import './routes/app_routes.dart';
 import './screens/splash_screen.dart';
